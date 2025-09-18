830a9392dfcb7814d1f4397005b3335e1470a932
eunseo <2023180012@tukorea.ac.kr>
2025-09-19 02:13:12 +0900

    지우개 생성, 출력 완료

--- CG_20250917/Tesk_05.cpp
+++ CG_20250917/Tesk_05.cpp
@@ -9,6 +9,9 @@ using namespace std;
 int width = 800;
 int height = 600;
 
+float recW = 0.06f;  // 사각형의 너비
+float recH = 0.08f;  // 사각형의 높이
+
 struct Rec
 {
     float color[3];
@@ -16,7 +19,8 @@ struct Rec
     float posX, posY;
     float width, height;
 };
-vector<Rec> rects;
+vector<Rec> rects;  // 사각형들
+Rec eraser;         // 지우개
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
@@ -26,7 +30,7 @@ void RandomColor(float color[3])
 {
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+    std::uniform_real_distribution<float> dis(0.01f, 1.0f);
     color[0] = dis(gen);
     color[1] = dis(gen);
     color[2] = dis(gen);
@@ -69,10 +73,84 @@ void InitRects()
         RandomPosition(temp.posX, temp.posY);
         RandomColor(temp.color);
         temp.scale = 1.0f;
-        temp.width = 0.06f;
-        temp.height = 0.08f;
+        temp.width = recW;
+        temp.height = recH;
         rects.push_back(temp);
     }
+}
+
+// 마우스 클릭한 좌표 정규화
+void PixelTrans(int px, int py, float& nx, float& ny)
+{
+    float w = (float)glutGet(GLUT_WINDOW_WIDTH);
+    float h = (float)glutGet(GLUT_WINDOW_HEIGHT);
+    nx = (px / w) * 2.0f - 1.0f;
+    ny = 1.0f - (py / h) * 2.0f;
+}
+
+// 중심 좌표 화면 밖으로 못 나가게
+void MouseInside(Rec& r)
+{
+    float w = (r.width * r.scale) * 0.5f;
+    float h = (r.height * r.scale) * 0.5f;
+
+    if (r.posX < -1.0f + w) r.posX = -1.0f + w;
+    if (r.posX > 1.0f - w) r.posX = 1.0f - w;
+    if (r.posY < -1.0f + h) r.posY = -1.0f + h;
+    if (r.posY > 1.0f - h) r.posY = 1.0f - h;
+}
+
+// 지우개 생성
+void InitEraser()
+{
+    eraser.color[0] = 0.0f;
+    eraser.color[1] = 0.0f;
+    eraser.color[2] = 0.0f;
+    eraser.scale = 1.0f;
+    eraser.width = recW * 2.0f;
+    eraser.height = recH * 2.0f;
+}
+
+// 지우개 그리기
+void DrawEraser()
+{
+    glPushMatrix();
+    glTranslatef(eraser.posX, eraser.posY, 0.0f);  // 사각형의 중심을 posX, posY로 이동
+    glScalef(eraser.scale, eraser.scale, 1.0f);
+    glColor3fv(eraser.color);
+    glRectf(
+        -eraser.width / 2, -eraser.height / 2,
+        eraser.width / 2, eraser.height / 2
+    );
+    glPopMatrix();
+}
+
+// 마우스 콜백
+void Mouse(int button, int state, int x, int y)
+{
+    float nx, ny;  // 마우스가 클릭한 좌표를 정규화
+    PixelTrans(x, y, nx, ny);
+
+    if (button == GLUT_LEFT_BUTTON)
+    {
+        if (state == GLUT_DOWN)
+        {
+			eraser.posX = nx;
+			eraser.posY = ny;
+			MouseInside(eraser);
+			InitEraser();
+            glutPostRedisplay(); // 화면 갱신
+        }
+        else if (state == GLUT_UP)
+        {
+            
+        }
+    }
+    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+    {
+       
+    }
+    glutPostRedisplay();
 }
 
 void main(int argc, char** argv)
@@ -97,6 +175,8 @@ void main(int argc, char** argv)
     glutDisplayFunc(drawScene);
     glutReshapeFunc(Reshape);
 
+    glutMouseFunc(Mouse);
+
     glutMainLoop();
 }
 
@@ -109,6 +189,7 @@ GLvoid drawScene()
     {
         DrawRect(rects[i]);
     }
+    DrawEraser();
 
     glutSwapBuffers();
 }
